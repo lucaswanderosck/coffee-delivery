1253fdfa77a2188d24397f43966b2da1cd2824d1
Lucas Wanderosck <lucaskleinwanderosck@gmail.com>
2024-03-04 14:51:37 -0300

    Adiciona página de sucesso e estilos relacionados

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,5 +1,49 @@
+import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react'
 import React from 'react'
+import successSvg from '../../assets/success.svg'
+import { Icon } from '../../components/Hero/styles'
+import { Container, Content, Info, OrderInfo } from './styles'
 
 export const Success: React.FC = () => {
-  return <></>
+  return (
+    <Container>
+      <h1>Uhu! Pedido confirmado</h1>
+      <p>Agora é só aguardar que logo o café chegará até você</p>
+
+      <Content>
+        <OrderInfo>
+          <div>
+            <Icon variant="purple">
+              <MapPin color="#fff" weight="fill" />
+            </Icon>
+            <Info>
+              <p>
+                Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                <p>Farrapos - Porto Alegre, RS</p>
+              </p>
+            </Info>
+          </div>
+          <div>
+            <Icon variant="yellow">
+              <Timer color="#fff" weight="fill" />
+            </Icon>
+            <Info>
+              <p>Previsão de Entrega</p>
+              <strong>20min - 30min</strong>
+            </Info>
+          </div>
+          <div>
+            <Icon variant="yellow-dark">
+              <CurrencyDollar color="#fff" />
+            </Icon>
+            <Info>
+              <p>Pagamento na Entrega</p>
+              <strong>Cartão de Crédito</strong>
+            </Info>
+          </div>
+        </OrderInfo>
+        <img src={successSvg} alt="" />
+      </Content>
+    </Container>
+  )
 }
---
+++ src/pages/Success/styles.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  max-width: 1120px;
+  width: 100%;
+  margin: 80px auto 0;
+
+  > h1 {
+    font-size: ${({ theme }) => theme.title.lg};
+    color: ${({ theme }) => theme.colors['yellow-dark']};
+    font-family: 'Baloo 2', sans-serif;
+  }
+
+  > p {
+    font-size: ${({ theme }) => theme.text.lg};
+    color: ${({ theme }) => theme.colors['base-subtitle']};
+  }
+`
+
+export const Content = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 5rem;
+`
+
+export const OrderInfo = styled.div`
+  width: 100%;
+  margin-top: 2.5rem;
+  padding: 2.5rem;
+  border-radius: 6px 36px;
+  border: 1px solid transparent;
+  background: ${({ theme }) => theme.colors.background};
+  background-clip: padding-box;
+  position: relative;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    margin: -1px;
+    border-radius: inherit;
+    background: ${({ theme }) =>
+      `linear-gradient(to bottom right, ${theme.colors.yellow}, ${theme.colors.purple})`};
+  }
+
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  > div {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+  }
+`
+
+export const Info = styled.div`
+  color: ${({ theme }) => theme.colors['base-text']};
+`
